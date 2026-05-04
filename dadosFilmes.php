a9f47c5a1a63a3f6070d30ad746335850e5d7d31
Frivcoom111 <carloseduardofaustino67@gmail.com>
2026-05-04 20:04:13 -0300

    Recurso: adicionar conjunto de dados inicial de filmes ao dadosFilmes.php

---
+++ dadosFilmes.php
@@ -0,0 +1,164 @@
+<?php
+
+$filmes = [
+    [
+        "id" => 1,
+        "titulo" => "The Shawshank Redemption",
+        "ano" => 1994,
+        "genero" => "Drama",
+        "diretor" => "Frank Darabont",
+        "nota" => 9.3
+    ],
+    [
+        "id" => 2,
+        "titulo" => "The Godfather",
+        "ano" => 1972,
+        "genero" => "Crime",
+        "diretor" => "Francis Ford Coppola",
+        "nota" => 9.2
+    ],
+    [
+        "id" => 3,
+        "titulo" => "The Dark Knight",
+        "ano" => 2008,
+        "genero" => "Ação",
+        "diretor" => "Christopher Nolan",
+        "nota" => 9.0
+    ],
+    [
+        "id" => 4,
+        "titulo" => "Pulp Fiction",
+        "ano" => 1994,
+        "genero" => "Crime",
+        "diretor" => "Quentin Tarantino",
+        "nota" => 8.9
+    ],
+    [
+        "id" => 5,
+        "titulo" => "Schindler's List",
+        "ano" => 1993,
+        "genero" => "Drama",
+        "diretor" => "Steven Spielberg",
+        "nota" => 8.9
+    ],
+    [
+        "id" => 6,
+        "titulo" => "Interstellar",
+        "ano" => 2014,
+        "genero" => "Ficção Científica",
+        "diretor" => "Christopher Nolan",
+        "nota" => 8.7
+    ],
+    [
+        "id" => 7,
+        "titulo" => "Fight Club",
+        "ano" => 1999,
+        "genero" => "Drama",
+        "diretor" => "David Fincher",
+        "nota" => 8.8
+    ],
+    [
+        "id" => 8,
+        "titulo" => "Forrest Gump",
+        "ano" => 1994,
+        "genero" => "Drama",
+        "diretor" => "Robert Zemeckis",
+        "nota" => 8.8
+    ],
+    [
+        "id" => 9,
+        "titulo" => "Inception",
+        "ano" => 2010,
+        "genero" => "Ficção Científica",
+        "diretor" => "Christopher Nolan",
+        "nota" => 8.8
+    ],
+    [
+        "id" => 10,
+        "titulo" => "The Matrix",
+        "ano" => 1999,
+        "genero" => "Ficção Científica",
+        "diretor" => "The Wachowskis",
+        "nota" => 8.7
+    ],
+    [
+        "id" => 11,
+        "titulo" => "Goodfellas",
+        "ano" => 1990,
+        "genero" => "Crime",
+        "diretor" => "Martin Scorsese",
+        "nota" => 8.7
+    ],
+    [
+        "id" => 12,
+        "titulo" => "The Silence of the Lambs",
+        "ano" => 1991,
+        "genero" => "Terror",
+        "diretor" => "Jonathan Demme",
+        "nota" => 8.6
+    ],
+    [
+        "id" => 13,
+        "titulo" => "Saving Private Ryan",
+        "ano" => 1998,
+        "genero" => "Guerra",
+        "diretor" => "Steven Spielberg",
+        "nota" => 8.6
+    ],
+    [
+        "id" => 14,
+        "titulo" => "The Lion King",
+        "ano" => 1994,
+        "genero" => "Animação",
+        "diretor" => "Roger Allers",
+        "nota" => 8.5
+    ],
+    [
+        "id" => 15,
+        "titulo" => "Parasite",
+        "ano" => 2019,
+        "genero" => "Drama",
+        "diretor" => "Bong Joon-ho",
+        "nota" => 8.5
+    ],
+    [
+        "id" => 16,
+        "titulo" => "Whiplash",
+        "ano" => 2014,
+        "genero" => "Drama",
+        "diretor" => "Damien Chazelle",
+        "nota" => 8.5
+    ],
+    [
+        "id" => 17,
+        "titulo" => "Gladiator",
+        "ano" => 2000,
+        "genero" => "Ação",
+        "diretor" => "Ridley Scott",
+        "nota" => 8.5
+    ],
+    [
+        "id" => 18,
+        "titulo" => "The Prestige",
+        "ano" => 2006,
+        "genero" => "Mistério",
+        "diretor" => "Christopher Nolan",
+        "nota" => 8.5
+    ],
+    [
+        "id" => 19,
+        "titulo" => "Joker",
+        "ano" => 2019,
+        "genero" => "Drama",
+        "diretor" => "Todd Phillips",
+        "nota" => 8.4
+    ],
+    [
+        "id" => 20,
+        "titulo" => "Django Unchained",
+        "ano" => 2012,
+        "genero" => "Western",
+        "diretor" => "Quentin Tarantino",
+        "nota" => 8.4
+    ],
+];
